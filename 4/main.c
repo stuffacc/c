c1dba0f103fbc7b15ed6282894ececbd8b589151
stuffacc <st141867@spbu.ru>
2025-10-12 14:56:54 +0300

    new output

--- 4/main.c
+++ 4/main.c
@@ -2,22 +2,27 @@
 
 void printArray(int size, int arr[]);
 void sortArray(int start, int end, int arr[]);
-
+int changes(int size, int arr1[], int arr2[]);
 
 void main() {
     int arr[100];
+    int before[100];
     int n = 0;
 
     while (n < 100 && scanf("%d", &arr[n]) == 1) {
+	before[n] = arr[n];
 	n++;
 	
 	if (getchar() == '\n') {
 	    break;
 	}
     }
+
     printArray(n, arr);
     sortArray(0, n, arr);
     printArray(n, arr);
+    
+    printf("%d %d\n", n, changes(n, before, arr));
 }
 
 
@@ -26,4 +31,15 @@ void printArray(int size, int arr[]) {
 	printf("%d ", arr[i]);
     }
     printf("\n");
+}
+
+int changes(int size, int arr1[], int arr2[]) {
+    int diff = 0;
+    for (int i = 0; i < size; i++) {
+	if (arr1[i] != arr2[i]) {
+		diff++;
+	}
+    }
+
+    return diff;
 }
