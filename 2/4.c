5ab3d4f3b04553036d87e86d199410cb87050208
stuffacc <st141867@spbu.ru>
2025-12-20 16:59:18 +0300

    format fix

--- 2/4.c
+++ 2/4.c
@@ -2,22 +2,21 @@
 
 void main() {
     int k = 0;
-    for (int num1 = 0; num1 <= 9; num1++) {
-    	for (int num2 = 0; num2 <= 9; num2++) {
-    	    for (int num3 = 0; num3 <= 9; num3++) {
-    
-    	        for (int num4 = 0; num4 <= 9; num4++) {
-    
-    	            for (int num5 = 0; num5 <= 9; num5++) {
-    
-    			for (int num6 = 0; num6 <= 9; num6++) {
+    int arr[28] = { 0 };
+    for (int i = 0; i <= 9; i++) {
+    	for (int j = 0; j <= 9; j++) {
+    	    for (int k = 0; k <= 9; k++) {
+    	        int sm = i + j + k;
+    	        arr[sm]++;
+    	    }
+    	}	
+    }
+    	        
+    	        
+    for (int i = 0; i < 28; i++) {
+        k += (arr[i]*arr[i]);
+    }
+    	        
 
-				int part1 = num1 + num2 + num3;
-				int part2 = num4 + num5 + num6;
-				if (part1 == part2) {
-					k = k + 1;
-				}
-}}}}}} //  
-
-printf("%d", k);
+    printf("%d\n", k);
 }
