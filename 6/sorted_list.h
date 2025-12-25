b894a4deb158ce8e3230b7bdb16d272502049361
stuffacc <st141867@spbu.ru>
2025-12-25 12:43:03 +0300

    sorted lisst update

--- 6/sorted_list.h
+++ 6/sorted_list.h
@@ -2,21 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 struct Node {
-        int value;
-        struct Node* next;
-        
+    int value;
+    struct Node* next;
+
 } typedef Node;
 
-
 struct List {
-        Node* head;
-        int size;
+    Node* head;
+    int size;
 } typedef List;
 
 void initList(List* list);
+Node* createNode(int value, Node* next);
+
 void addValue(List* list, int value);
 void removeValue(List* list, int value);
+
 void printList(List* list);
